d94b2b83d3cfa8405341d98286b2a31b8c7f9a25
Milena Vitoria dos Santos Golnik <141414014+milenagolnik@users.noreply.github.com>
2023-08-11 08:18:25 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(600, 600);
+ background("green");
+}
+
+function draw() {
+  
+  
+  stroke("blue");
+  fill("yellow");
+  
+if(mouseIsPressed){rect(mouseX,mouseY,60,60);}
+
+
+}
